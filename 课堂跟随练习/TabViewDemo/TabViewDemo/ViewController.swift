52fb9cccf49c60a7a94878f6f2d11aa477e38518
mrwang90hou <mrwang90hou@163.com>
2018-05-15 17:13:33 +0800

    TabViewDemo

---
+++ 课堂跟随练习/TabViewDemo/TabViewDemo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  TabViewDemo
+//
+//  Created by 王宁 on 2017/4/20.
+//  Copyright © 2017年 mrwang90hou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    
+    /*只读图片的形状，不读颜色
+     1.png格式
+     2.颜色也可以显示
+ 
+ */
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
