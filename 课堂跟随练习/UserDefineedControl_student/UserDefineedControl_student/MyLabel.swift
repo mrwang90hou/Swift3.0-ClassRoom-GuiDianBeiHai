af2ce075b23b5b6656250e8cee7a8b0bf262e48b
mrwang90hou <mrwang90hou@163.com>
2018-05-15 17:14:37 +0800

    UserDefineedControl_student

---
+++ 课堂跟随练习/UserDefineedControl_student/UserDefineedControl_student/MyLabel.swift
@@ -0,0 +1,27 @@
+//
+//  MyLabel.swift
+//  UserDefineedControl_student
+//
+//  Created by 王宁 on 2017/5/11.
+//  Copyright © 2017年 mrwang90hou. All rights reserved.
+//
+
+import UIKit
+
+class MyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+    }
+    let context = UIGraphicsCurrentContext()
+    context?.set
+    
+}
