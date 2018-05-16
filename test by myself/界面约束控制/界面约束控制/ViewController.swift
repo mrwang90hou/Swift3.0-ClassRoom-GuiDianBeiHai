c49a1df027ff71b9464249619ade220185b5d006
mrwang90hou <mrwang90hou@163.com>
2018-05-16 20:08:23 +0800

    界面约束控制

---
+++ test by myself/界面约束控制/界面约束控制/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  界面约束控制
+//
+//  Created by 王宁 on 2017/4/26.
+//  Copyright © 2017年 mrwang90hou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
